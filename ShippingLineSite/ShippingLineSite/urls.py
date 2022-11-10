91bff49bd474055576d9300accaa360989174980
Max Shchur <mxm.shchur@gmail.com>
2022-11-10 21:21:30 +0200

    made views for JWT token obtain

--- ShippingLineSite/ShippingLineSite/urls.py
+++ ShippingLineSite/ShippingLineSite/urls.py
@@ -1,9 +1,17 @@
-
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django_backend import views
+from rest_framework.routers import DefaultRouter
+
+
+from rest_framework_simplejwt.views import TokenRefreshView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('', views.getRoutes),
+    path('token/', views.MyTokenObtainPairView.as_view(), name='token_obtain'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
-]# from ..django_backend import views
+]
 
--- ShippingLineSite/django_backend/views.py
+++ ShippingLineSite/django_backend/views.py
@@ -1,10 +1,35 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 
-def index(request):
-    msg = 'Start Page'
-    return HttpResponse(msg)
 
-def hello(request):
-    msg = 'Hi'
-    return HttpResponse( msg)
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Allows us to encrypt additional fields
+        token['username'] = user.username
+
+        return token
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+
+# Probably will need in future
+# @api_view(['GET'])
+# def getRoutes(request):
+#     routes = [
+#         '/api/token',
+#         'api/token/refresh',
+#     ]
+#
+#     return Response(routes)
+
+
+
+
+
