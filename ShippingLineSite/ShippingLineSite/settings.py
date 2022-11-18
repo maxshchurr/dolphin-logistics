1b7b223a97bf29e8b5cab5a820ab205c04040690
Max Shchur <mxm.shchur@gmail.com>
2022-11-18 20:33:11 +0200

    small fixes in rest_framework

--- ShippingLineSite/ShippingLineSite/settings.py
+++ ShippingLineSite/ShippingLineSite/settings.py
@@ -36,9 +36,12 @@ INSTALLED_APPS = [
 
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES':{
-        'rest_framework-simplejwt.authentication.JWTAuthentication',
-    }
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
 }
 
 SIMPLE_JWT = {
